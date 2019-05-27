936e0b68aa9db82ff50a785c778cc7fb6f2f2260
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-27 14:26:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function BoardMember (name, homeState, training) {
   }
   
   this.releasePressStatement = function() {
-    return "You will see great things from Scuber"
+    return "You will see great things from Scuber."
   }
 }
